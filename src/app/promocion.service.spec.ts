d8ae08ae9fd191523301909ceb8d5a8fee6c4c4f
Luis Velasquez <luis.velasquezg@udea.edu.co>
2020-04-13 17:41:22 -0500

    Prueba Servicio Promocion

--- src/app/promocion.service.spec.ts
+++ src/app/promocion.service.spec.ts
@@ -1,20 +1,21 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 import { PromocionService } from './promocion.service';
 import { componentFactoryName } from '@angular/compiler';
 import { throwError } from 'rxjs';
 
-describe('PromocionService', () => {
-  let service: PromocionService;
+// describe('PromocionService', () => {
+//   let service: PromocionService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(PromocionService);
-  });
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({});
+//     service = TestBed.inject(PromocionService);
+//   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+//   it('should be created', () => {
+//     expect(service).toBeTruthy();
+//   });
 
   // describe('When getPromociones() is called', () => {
   //   it('should handle error', () => {
@@ -24,4 +25,53 @@ describe('PromocionService', () => {
   //   });
   // });
 
+
+describe('PromocionService', () => {
+
+  const mockResponse = [
+    {
+      "codigo": 1,
+      "mensaje": "Usted puede aplicar 10.0% de descuento al producto Loratadina",
+      "fecha": "2020-04-13T21:35:01.645+0000",
+      "producto": {
+        "codigo": 1,
+        "nombre": "Loratadina",
+        "precio": 1200,
+        "costo": 1000,
+        "iva": 0.16,
+        "inventario": 300,
+        "categoria": {
+          "codigo": 1,
+          "nombre": "Antistamínicos"
+        }
+      },
+      "porcentajePromocion": 0.1
+    }];
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PromocionService]
+    });
+  });
+
+  describe('get data', () => {
+    it('should get results',
+    inject([HttpTestingController, PromocionService], (httpMock: HttpTestingController, promocionServiceTested: PromocionService) => {
+      const apiUrl = 'http://192.168.137.1:8080/promociones/verpromociones/';
+      promocionServiceTested.getPromociones()
+      .subscribe(
+        (data) => {
+          expect(data).toEqual(mockResponse);
+        }
+      );
+      const req = httpMock.expectOne(apiUrl);
+      expect(req.request.method).toBe('GET');
+      req.flush(mockResponse);
+    })
+  );
+  });
+
 });
+
+
--- src/app/promocion.service.ts
+++ src/app/promocion.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
@@ -12,7 +13,7 @@ export class PromocionService {
 
   constructor(protected http: HttpClient) { }
 
-  getPromociones() {
+  getPromociones(): Observable<any> {
     return this.http.get(this.urlAPI);
   }
 
